8a917ede49808f1525e7d9472fbf2dda148023f3
Eko Andri Prasetyo <eko.andri@prasetyo.co>
2025-12-31 12:52:10 +0700

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,16 +11,16 @@ module.exports = {
         PORT: "5000",
 
         // JWT (nama harus PERSIS ini)
-        ACCESS_TOKEN_KEY: "f518bcb858699dc4514b5276dc9238c2729d4d133c2bb89f1919af206614b2d6",
-        REFRESH_TOKEN_KEY: "78105eb7f668d9595eb67547420990377e53774bc64338d9096bacdd998b149f",
+        ACCESS_TOKEN_KEY: "access_token_key",
+        REFRESH_TOKEN_KEY: "refresh_token_key",
         ACCESS_TOKEN_AGE: "1800",
 
         // DB Aiven
-        DATABASE_URL: "postgres://avnadmin:AVNS_3vxLaq8JJ3C3gAC0jJd@fashiondb-ekoandriprasetyo.k.aivencloud.com:22503/forumapi?sslmode=require",
-        PGHOST: "fashiondb-ekoandriprasetyo.k.aivencloud.com",
-        PGPORT: "22503",
-        PGUSER: "avnadmin",
-        PGPASSWORD: "AVNS_3vxLaq8JJ3C3gAC0jJd",
+        DATABASE_URL: "postgres://USER:PASSWORD@HOST:PORT/forumapi?sslmode=require",
+        PGHOST: "HOST",
+        PGPORT: "PORT",
+        PGUSER: "USER",
+        PGPASSWORD: "PASSWORD",
         PGDATABASE: "forumapi",
         PGSSLMODE: "require",
       },
